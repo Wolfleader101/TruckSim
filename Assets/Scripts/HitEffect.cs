7b7a192bd4ac30b2a62fac5d7a9fa66486e0ece6
Wolfleader <james.morgan@iinet.net.au>
2021-05-16 17:59:49 +0800

    re visiting old project

---
+++ Assets/Scripts/HitEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour
+{
+    public GameObject bloodFX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            BloodEffect();
+        }
+    }
+
+    void BloodEffect()
+    {
+        GameObject blood = Instantiate(bloodFX, this.transform.position, this.transform.rotation);
+        blood.GetComponent<ParticleSystem>().Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
